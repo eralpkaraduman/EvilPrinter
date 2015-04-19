dacb71232b199f72565a7b29ca7873e2315ae11c
Eralp Karaduman <eralpkaraduman@gmail.com>
2015-04-20 02:50:47 +0300

    proto lean

--- LD48-32-Unity/Assets/Scripts/Printer.cs
+++ LD48-32-Unity/Assets/Scripts/Printer.cs
@@ -9,12 +9,21 @@ public class Printer : MonoBehaviour {
 	public GameObject dragLine;
 	public GameObject body;
 	public Transform bodyPivot;
+	public Transform cameraPivot;
+	public Transform bodyLeanPivot;
+
+	public float maxDragDist = 12;
+	public float minDragDist = 2;
+
+	public float maxLean = 12;
+
 
 	private Vector3 dragStartPos;
-	public Vector3 dragStartPosOffset = Vector3.zero;
+	//public Vector3 dragStartPosOffset = Vector3.zero;
 
 	public bool debugMouseCursorVisible = true;
 
+	private bool dragging = false;
 
 	private class MouseResult : Object{
 		public Vector3 mousePos;
@@ -31,7 +40,7 @@ public class Printer : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		dragStartPos = this.transform.position;
-		dragStartPos += dragStartPosOffset;
+		//dragStartPos += dragStartPosOffset;
 		CursorPlane = new Plane (CursorPlaneObject.transform.position,Vector3.up);
 
 		Cursor.GetComponent<Renderer>().enabled = debugMouseCursorVisible;
@@ -41,31 +50,52 @@ public class Printer : MonoBehaviour {
 	void Update () {
 		MouseResult mouseRes = this.mousePos ();
 
+		float dragDistance = Vector3.Distance(dragStartPos,mouseRes.mousePos);
+
+		float percent_drag = (dragDistance - minDragDist) / (maxDragDist - minDragDist);
+		percent_drag = Mathf.Max (0, percent_drag);
+		percent_drag = Mathf.Min (1, percent_drag);
+
+		// dragline
 		Cursor.transform.position = mouseRes.mousePos;
-
 		Vector3 mid = (mouseRes.mousePos -dragStartPos)/2;
-
-		//Vector3 dragLinePos = mouseRes.ray.GetPoint (mouseRes.distance / 2);
-
 		dragLine.transform.localPosition = mid;
 		dragLine.transform.LookAt (dragStartPos);
-
 		Vector3 dragLineScale = dragLine.transform.localScale;
-		dragLineScale.z = Vector3.Distance(dragStartPos,mouseRes.mousePos);
+		dragLineScale.z = dragDistance;
 		dragLine.transform.localScale = dragLineScale;
 
+		// aim
 		Vector3 bodyLookTarget = body.transform.position;
 		bodyLookTarget.z = mouseRes.mousePos.z;
 		bodyLookTarget.x = mouseRes.mousePos.x;
+
+		float lean = 0;
+
+		if (dragging) {
+			lean = -percent_drag*maxLean;
+		} else {
+			Vector3 forward = bodyPivot.transform.position;
+			forward.z -= 10;
+			bodyLookTarget = forward;
+		}
+
+		dragLine.GetComponent<Renderer>().enabled = dragging;
 		bodyPivot.LookAt (bodyLookTarget);
+
+		// lean
+		Vector3 leanRotation = bodyLeanPivot.eulerAngles;
+		leanRotation.x = lean;
+		bodyLeanPivot.transform.eulerAngles = leanRotation;
+
 	}
 
 	void OnMouseDown(){
-
+		dragging = true;
 	}
 
 	void OnMouseUp(){
-
+		dragging = false;
 	}
 
 	MouseResult mousePos(){
