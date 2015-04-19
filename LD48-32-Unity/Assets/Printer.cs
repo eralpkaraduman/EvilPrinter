04ec520e1606265cfaaa01fab51da0a0f7a254b8
Eralp Karaduman <eralpkaraduman@gmail.com>
2015-04-19 21:08:39 +0300

    desks

---
+++ LD48-32-Unity/Assets/Printer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Printer : MonoBehaviour {
+
+	public GameObject CursorPlaneObject = null;
+	public GameObject Cursor = null;
+	public Plane CursorPlane;
+
+	public Vector3 dragStartPos = Vector3.zero;
+
+
+	// Use this for initialization
+	void Start () {
+		CursorPlane = new Plane (CursorPlaneObject.transform.position,Vector3.up);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Cursor.transform.position = MousePos ();
+	}
+
+	void OnMouseDown(){
+
+	}
+
+	void OnMouseUp(){
+
+	}
+
+	Vector3 MousePos(){
+
+		float dist;
+
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit hit = new RaycastHit();
+
+		if (CursorPlaneObject.GetComponent<Collider>().Raycast (ray,out hit, 3000.0f)) {
+
+			return ray.GetPoint(hit.distance);
+		}
+		return dragStartPos;
+
+
+		Vector3 mousePoint = Camera.main.ScreenToViewportPoint (Input.mousePosition);
+		return mousePoint;
+	}
+}
---
+++ LD48-32-Unity/Assets/Scripts/SideRunner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideRunner : MonoBehaviour {
+
+	public float speed = 5.0f;
+	public float endPos = 24.0f;
+	public float startPos = -24.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		Reset ();
+	}
+
+	void Reset(){
+		Vector3 pos = transform.position;
+		pos.x = startPos;
+		transform.position = pos;
+	}
+	
+
+	void Update () {
+
+		if (transform.position.x >= endPos) {
+			Reset();
+			return;
+		}
+
+		transform.Translate(Vector3.right * Time.deltaTime * speed);
+	}
+}
