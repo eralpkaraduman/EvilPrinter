06ac3938dd03cc4cdb3c6ec94be497401d3d7c12
Eralp Karaduman <eralpkaraduman@gmail.com>
2015-04-20 19:55:12 +0300

    add paper

---
+++ LD48-32-Unity/Assets/PaperShooter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaperShooter : MonoBehaviour {
+
+	public GameObject paperPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void shoot(){
+		
+	}
+}
